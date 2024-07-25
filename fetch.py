7464198a1c5be0fcdbd5467b8d802aec4f33439d
slashbee <babu@slashb.net>
2024-07-25 17:15:40 +0530

    Create fetch.py
    Adding the skeleton Python script to fetch Twitter feed and posts.

---
+++ fetch.py
@@ -0,0 +1,47 @@
+import tweepy
+
+# Fill the credentials below
+
+consumer_key = "Xgd746y52YgtUNdN72AHRJYwa" 
+consumer_secret = "VmTVZaLOdTvLvTRXCch6q4pygHU3LYvWCJIr7bwL23fehjeM87"
+access_key = "1815656230125445120-QAY75PRjiJXSQarFmJb05oq2D31DUh"
+access_secret = "r5KW1FJl0BF60xQdhyKCynlXhgLksneKwnl14zVmKrupN"
+
+# Function to extract tweets
+def get_tweets(username):
+		
+		# Authorization to consumer key and consumer secret
+		auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+
+		# Access to user's access key and access secret
+		auth.set_access_token(access_key, access_secret)
+
+		# Calling api
+		api = tweepy.API(auth)
+
+		# 200 tweets to be extracted
+		number_of_tweets=200
+		tweets = api.user_timeline(screen_name=username)
+
+		# Empty Array
+		tmp=[] 
+
+		# create array of tweet information: username, 
+		# tweet id, date/time, text
+		tweets_for_csv = [tweet.text for tweet in tweets] # CSV file created 
+		for j in tweets_for_csv:
+
+			# Appending tweets to the empty array tmp
+			tmp.append(j) 
+
+		# Printing the tweets
+		print(tmp)
+
+
+# Driver code
+if __name__ == '__main__':
+
+	# Here goes the twitter handle for the user
+	# whose tweets are to be extracted.
+	get_tweets("gcc_x_sa") 
+
